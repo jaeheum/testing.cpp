0bd5267f23113038d9c64b40ca27d8ec0132ae4a
Vesa Karvonen <vesa.a.j.k@gmail.com>
2019-05-04 23:44:29 +0300

    Doc comments

--- provides/include/testing_v1/synopsis.hpp
+++ provides/include/testing_v1/synopsis.hpp
@@ -6,11 +6,15 @@ namespace testing_v1 {
 
 // test.hpp ====================================================================
 
+/// Holds a registered test action and should only be used at namespace scope.
 template <class Action> struct test_t;
 
+/// Creates and registers a test action and should only be used at namespace
+/// scope.
 template <class Action>
 test_t<std::remove_cvref_t<Action>> test(Action &&action);
 
+/// Prints `FAIL` and `exit(1)`s the program if the result is false.
 void verify(bool result);
 
 } // namespace testing_v1
